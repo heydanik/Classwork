b2cb162bb8ee7f13b85c02109f5f2d7cb7311a38
heydanik <khilkevich.d@gmail.com>
2016-03-14 17:38:34 -0400

    Update jquery.js

--- Week6/js/jquery.js
+++ Week6/js/jquery.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function () {
-    var addItem = $('#addItem');
-    var removeItem = $('#removeItem');
-    addItem.click(function () {
-        var toAdd = $('input#text').val();
-        $('#list').append('<li><input type="checkbox"/>' + toAdd + '</li>');
-    });
-    removeItem.click(function () {
-        var toRemove = $('input#text').val();
-        $('li:checked').remove();
-    });
-});
-=======
 $(document).ready(function() {
 
 function addListItem() {
@@ -73,4 +59,3 @@ $(function() {
      
 
 })
->>>>>>> origin/gh-pages

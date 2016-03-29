e02a65f9193b6b7d77b2d677f227086ef631dca3
heydanik <khilkevich.d@gmail.com>
2016-03-28 22:45:59 -0400

    Added midterm

---
+++ midterm/js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $("select").val('');
+    $("#page").hide();
+    
+      $("#btn").click(function() {
+
+    // grab the values from the input boxes, then append them to the DOM
+    $("#one").append($("#first").val());
+    $("#two").append($("#second").val());
+    $("#three").append($("#third").val());
+    $("#four").append($("#fourth").val());      
+    
+    $("#six").append($("#sixth").val());
+    $("#seven").append($("#seventh").val());
+    $("#eigh").append($("#eighth").val());
+    $("#nine").append($("#ninth").val());
+    $("#ten").append($("#tenth").val());
+
+    $("#page").show();
+
+    // empty the form's values
+   
+    $("#input").hide();
+      
+
+
+  });
+
+$("#playAgain").click(function(){
+ 
+    $("#input").show();
+    $("#page").hide();
+    $(".span").empty();
+    $("select").val('');
+    
+});
+
+});
+    
